549af78085a9697dacd9ea6c91d1746fc1194c16
amkloss <hiw4tt@gmail.com>
2023-11-24 12:05:04 +0100

    Test-list

---
+++ test-list.js
@@ -0,0 +1,13 @@
+// A list of things is called an array in JavaScript
+let names = [
+    'Anna',
+    'Bertil',
+    'Cecilia',
+    'David'
+  ];
+  
+  // Loop through all names in the name list/array
+  // and say hi to each item in the list
+  for (let name of names) {
+    console.log('Hej ' + name + '!');
+  }
